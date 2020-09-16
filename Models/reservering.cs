d4853f0b0f3158bb70fd27aca3e0bb8fa52751bd
Tjips <47105980+Tjips@users.noreply.github.com>
2020-09-16 10:22:55 +0200

    project aangemaakt
    Kaas

---
+++ Models/reservering.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodTimes.Models
+{
+    public class reservering
+    {
+        public int reserveringsID { get; set; }
+        public string voornaam { get; set; }
+        public string achternaam { get; set; }
+        public string email { get; set; }
+        public string telefoon { get; set; }
+        public string opmerking { get; set; }
+        public DateTime datum { get; set; }
+        public string begintijd { get; set; }
+        public string eindtijd { get; set; }
+    }
+}
